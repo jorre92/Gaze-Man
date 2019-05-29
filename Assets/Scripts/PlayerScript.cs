9388fe64adafb55f03746f8809cf73bfe74f10d3
Johannes Eriksson Wallin <jewn@tobiidynavox.com>
2019-05-29 14:49:30 +0200

    Eyetracking added to level one

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var gazePoint = TobiiAPI.GetGazePoint();
+        Debug.Log("hi " + gazePoint.Screen);
+        //var ray = camera.ScreenPositionToRay(gazePoint.Screen);
+
+        //if (Physics.Raycast(ray, out hit))
+        {
+            // do stuff.
+        }
+    }
+}
